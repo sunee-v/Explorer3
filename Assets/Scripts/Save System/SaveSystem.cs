34dc28d6eed75b3f9f40171af14749e75f50ffe7
Venus <36venu.pn@gmail.com>
2023-03-30 23:50:58 -0400

    saveAndLoad

--- Assets/Scripts/Save System/SaveSystem.cs
+++ Assets/Scripts/Save System/SaveSystem.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
 
 public class SaveSystem : MonoBehaviour
 {
+    private string savePath;
+    private string key = "mySecretKey";
+    private string encryptedJson;
+
+    PlayerMovement playerLocation;
     // Start is called before the first frame update
     void Start()
     {
-        
+        savePath = Application.persistentDataPath + "/save.json";
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    void OnApplicationQuit()
     {
-        
+        // Serialize to JSON
+        string json = JsonUtility.ToJson(playerLocation);
+        encryptedJson = encryptAndDecrypt(json, key);
+        //File.WriteAllText(savePath, encryptedJson);
+
+    }
+  
+    public PlayerMovement Load()
+    {
+        //string encryptedJson = File.ReadAllText(savePath);
+        string json = encryptAndDecrypt(encryptedJson, key);
+        return new PlayerMovement();
+    }
+
+    private string encryptAndDecrypt(string text, string key)
+    {
+        var result = new StringBuilder();
+        for (int c = 0; c < text.Length; c++)
+            result.Append((char)((uint)text[c] ^ (uint)key[c % key.Length]));
+        return result.ToString();
     }
 }
